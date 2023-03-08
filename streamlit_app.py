952cb56d5b1684bde2e16db5fe80e3fa98d95b75
Nandhini Kannan <93576231+kannannandhini17@users.noreply.github.com>
2023-03-08 12:22:47 +0530

    Update streamlit_app.py
    mutiselect options added

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,5 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
