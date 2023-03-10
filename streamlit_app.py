442e644bc6dea93cc7fae7afef2b4797d00208ee
Nandhini Kannan <93576231+kannannandhini17@users.noreply.github.com>
2023-03-10 21:29:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,20 +31,32 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select the fruit for information.")
+    else:
+      fruityvice_response = response.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+ 
+except URLError as e:
+  streamlit.error()
+
+##fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+##streamlit.text(fruityvice_response)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+##streamlit.header('Fruityvice Fruit Juice')
+
+##fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
-streamlit.header('Fruityvice Fruit Juice')
+##fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+##streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
+streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
@@ -76,3 +88,6 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+      
+streamlit.write('The user entered ', fruit_choice)
