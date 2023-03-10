c82e06db97294aafd4540b1b98c3ba007d5af0ff
Nandhini Kannan <93576231+kannannandhini17@users.noreply.github.com>
2023-03-10 17:52:37 +0530

    Update streamlit_app.py
    fruityvice normalized instances creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,6 @@ streamlit.header('Fruityvice Fruit Juice')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.text(fruityvice_normalized)
